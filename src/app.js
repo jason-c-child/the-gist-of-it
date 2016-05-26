c98411a61f5f44d0fc49555c3d476bb0533d4192
jason-c-child <child.jason@icloud.com>
2016-05-26 12:24:32 -0700

    chore: remove cruft from local dev

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import fetch from 'node-fetch'
 import _ from 'lodash'
 const URL = 'https://gist.githubusercontent.com/ktilcu/ef1d416279e453389c5d4cf1e6fb708b/raw/160782d79e83b64da142969ccaa7f9cf1fa16e01/CreativeFamily.json'
 
-import seed from '../util/dataSeed.json'
-
 const getWidgets = url => fetch(url).then(response => response.json()).catch(err => console.log('error fetching stuff!!', err))
 
 const extractWidgets = data => _.flattenDeep(_.concat(_.keys(seed.sizes).map(k => seed.sizes[k]['frames'].map(frame => frame.widgets), _.keys(seed.sizes).map(k => seed.sizes[k]['background']['widgets']))))
