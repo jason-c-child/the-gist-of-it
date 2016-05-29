0c72537bb00dfc1cf8cec0ad65e56493f22a0e2b
jason-c-child <child.jason@icloud.com>
2016-05-28 19:08:30 -0700

    feat: add prompt; add motd; pretty print json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 // sample URL https://gist.githubusercontent.com/ktilcu/ef1d416279e453389c5d4cf1e6fb708b/raw/160782d79e83b64da142969ccaa7f9cf1fa16e01/CreativeFamily.json
 import fetch from 'node-fetch'
 import _ from 'lodash'
-
+const PROMPT = 'URL > '
+const MOTD = ['Avalanche CreativeFamily Widget Flattener',' ','Enter a REST URL endpoint which will return a CreativeFamily JSON file.']
 const getWidgets = url => fetch(url).then(response => response.json()).catch(err => console.log('error fetching stuff!!', err))
 
 const extractWidgets = data => _.flattenDeep(_.concat(_.keys(data.sizes).map(k => data.sizes[k]['frames'].map(frame => frame.widgets), _.keys(data.sizes).map(k => data.sizes[k]['background']['widgets']))))
@@ -14,8 +15,10 @@ const flattenWidgets = data => {
 }
 
 process.stdin.on('readable', () => {
+  MOTD.map(m => process.stdout.write(m+'\n'))
+  process.stdout.write(PROMPT)
   const partial = process.stdin.read()
   partial !== null && getWidgets(partial.toString())
     .then(json => flattenWidgets(json))
-    .then(v => console.log(v))
+    .then(v => console.log(JSON.stringify(v, null, 2)))
 })
