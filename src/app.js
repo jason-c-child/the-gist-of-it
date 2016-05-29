b9b1b8935c3aea50908a40e662b4d5ba10550e1e
jason-c-child <child.jason@icloud.com>
2016-05-28 20:46:53 -0700

    feat: refactor to use async/await
    async/await via generators :fireworks:

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
 // sample URL https://gist.githubusercontent.com/ktilcu/ef1d416279e453389c5d4cf1e6fb708b/raw/160782d79e83b64da142969ccaa7f9cf1fa16e01/CreativeFamily.json
+import 'babel-polyfill'
 import minimist from 'minimist'
-import {getWidgets, flattenWidgets} from './widgetHandler'
+import {flattenWigdets} from './widgetHandler'
 import {infoLogger} from './logger'
-import {PROMPT, MOTD} from './config'
+import {PROMPT, MOTD, HELP} from './config'
 
 
-const main = () => {
+const main = async () => {
   const _args = minimist(process.argv.slice(2))
-  if(_args.url) {
-    getWidgets(_args.url)
-    .then(json => flattenWidgets(json))
-  } else {
-    
+  if(_args.help) {
+    HELP.map(m => infoLogger(m))
+    process.exit(0)
   }
-  process.stdin.on('readable', () => {
+  if(_args.url) {
+    if (_args.url === 'sample') _args.url = `https://gist.githubusercontent.com/ktilcu/ef1d416279e453389c5d4cf1e6fb708b/raw/160782d79e83b64da142969ccaa7f9cf1fa16e01/CreativeFamily.json`
+    const _widgets = await flattenWigdets(_args.url)
+    infoLogger(JSON.stringify(_widgets, null, 2))
+  } else {
     MOTD.map(m => process.stdout.write(m+'\n'))
     process.stdout.write(PROMPT)
-    const partial = process.stdin.read()
-    partial !== null && getWidgets(partial.toString())
-    .then(json => flattenWidgets(json))
-    .then(v => infoLogger(JSON.stringify(v, null, 2)))
-  })
+    process.stdin.on('readable', async () => {
+      const partial = process.stdin.read()
+      if(partial !== null) {
+        const _widgets = await flattenWigdets(partial.toString())
+        infoLogger(JSON.stringify(_widgets, null, 2))
+      }
+    })
+  }
 }
 
 main()
--- src/config.js
+++ src/config.js
@@ -1,6 +1,17 @@
+import _ from 'lodash'
+
 export const PROMPT = 'URL > '
 export const MOTD = [
   'Avalanche CreativeFamily Widget Flattener',
-  ' ',
-  'Enter a REST URL endpoint which will return a CreativeFamily JSON file.'
+  `v${process.env.npm_package_version}`,
+  '',
+  'Enter a REST URL endpoint which will return a CreativeFamily JSON file',
+  ''
 ]
+export const HELP = _.concat(MOTD, [
+  'syntax:',
+  '\tnpm run start -- [options]',
+  'options: ',
+  `\t--url <CreativeFamily JSON url || 'sample'>`,
+  '\t--help'
+])
