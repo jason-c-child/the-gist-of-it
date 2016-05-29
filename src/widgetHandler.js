606ef75464b3e182f29260037ec6e3348b064004
jason-c-child <child.jason@icloud.com>
2016-05-28 22:44:27 -0700

    feat: add some tests

--- src/widgetHandler.js
+++ src/widgetHandler.js
@@ -4,22 +4,17 @@ import {errLogger} from './logger'
 import _ from 'lodash'
 
 export const flattenWigdets = async url => {
-  const _widgets = await getWidgets(url)
-  const _flattened = await _flattenWidgets(_widgets)
-  return _flattened
+  const _json = await getWidgets(url)
+  const _widgets = await extractWidgets(_json)
+  return await _flattenWidgets(_json.assets, _widgets)
 }
 
-const _flattenWidgets = data => {
-    const assets = data.assets
-    const widgets = extractWidgets(data)
-
-    const _flattened = _.flatten(assets.map(asset => _.filter(widgets, widget => widget['asset-uuid'] === asset.uuid).map(pairing => {return {...asset, ...pairing}})))
-    return Promise.resolve(_flattened)
-}
-
+export const _flattenWidgets = (assets, widgets) => 
+  Promise.resolve(_.flatten(assets.map(asset => _.filter(widgets, widget => widget['asset-uuid'] === asset.uuid).map(pairing => {return {...asset, ...pairing}}))))
+  
 export const getWidgets = url => fetch(url).then(response => response.json()).catch(err => errLogger(err))
 
-const extractWidgets = data => {
+export const extractWidgets = data => {
     const _keys = _.keys(data.sizes)
     return _.flattenDeep(_.concat(_keys.map(k => data.sizes[k]['frames'].map(frame => frame.widgets), _keys.map(k => data.sizes[k]['background']['widgets']))))
 }
