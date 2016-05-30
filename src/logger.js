2e318cc8a680f85550bae8aa72c96a675e6af8c1
jason-c-child <child.jason@icloud.com>
2016-05-29 18:19:35 -0700

    feat: add sentry support for exception logging

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,14 @@
 import chalk from 'chalk'
+import raven from 'raven'
+
+const sentry = new raven.Client('https://7e9d3d30e408465d813cab41e2a0d753:35e67248e8fc46af97bc1e0cf89a35f2@app.getsentry.com/80477')
+
 const error = chalk.bold.red
 const info = chalk.bold.green
 
-export const errLogger = err => console.error(error(`${err}`))
+export const errLogger = err => {
+  console.error(error(`${err}`))
+  sentry.captureException(err)
+}
 
 export const infoLogger = i => console.info(info(i))
