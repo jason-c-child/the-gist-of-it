cb3a1176632c413cc7eba719fbf5b2c8862abd34
jason-c-child <child.jason@icloud.com>
2016-06-07 18:37:34 -0700

    feat: implement as webservice
    implement as a webservice, in this case using koa
    
    service implements an endpoint `url:port/flatten` and accepts a JSON payload (creativefamily)
    and returns the flattened widgets as a JSON payload
    
    returns an empty object if no flattenable widgets were found

--- src/widgetHandler.js
+++ src/widgetHandler.js
@@ -1,18 +1,13 @@
 import 'babel-polyfill'
-import fetch from 'node-fetch'
-import {errLogger} from './logger'
 import _ from 'lodash'
 
-export const flattenWigdets = async url => {
-  const _json = await getWidgets(url)
+export const flattenWigdets = async _json => {
   const _widgets = await extractWidgets(_json)
   return await _flattenWidgets(_json.assets, _widgets)
 }
 
 export const _flattenWidgets = (assets, widgets) => 
   Promise.resolve(_.flatten(assets.map(asset => _.filter(widgets, widget => widget['asset-uuid'] === asset.uuid).map(pairing => {return {...asset, ...pairing}}))))
-  
-export const getWidgets = url => fetch(url).then(response => response.json()).catch(err => errLogger(err))
 
 export const extractWidgets = data => {
     const _keys = _.keys(data.sizes)
